80d081e3248f032c48855643d3bf13dec7ad633d
youssef hoummad <youssef.hoummad@outlook.com>
2020-12-19 18:52:35 +0100

    Add files via upload

---
+++ build/precache-manifest.52b19aa15148a5f319a3da24d242e6d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9ddda10733495708add1d2c698151b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ad9aa63fe36337be23",
+    "url": "/static/css/2.f9a0735a.chunk.css"
+  },
+  {
+    "revision": "70e1b0b587d1ca07bc7c",
+    "url": "/static/css/main.bc9bf328.chunk.css"
+  },
+  {
+    "revision": "f4ad9aa63fe36337be23",
+    "url": "/static/js/2.8c054bc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c054bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e1b0b587d1ca07bc7c",
+    "url": "/static/js/main.57d9ef82.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
