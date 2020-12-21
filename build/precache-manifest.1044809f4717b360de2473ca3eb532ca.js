3e94ece19f242ece3587c3c478c8e338fab7a63c
youssef hoummad <youssef.hoummad@outlook.com>
2020-12-21 17:21:49 +0100

    new build v 0.2

---
+++ build/precache-manifest.1044809f4717b360de2473ca3eb532ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8977cf72581a1cb41a8c48d4827787da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ad9aa63fe36337be23",
+    "url": "/static/css/2.f9a0735a.chunk.css"
+  },
+  {
+    "revision": "3334885e7dcbdd13efc9",
+    "url": "/static/css/main.e056542b.chunk.css"
+  },
+  {
+    "revision": "f4ad9aa63fe36337be23",
+    "url": "/static/js/2.8c054bc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c054bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3334885e7dcbdd13efc9",
+    "url": "/static/js/main.bf363cba.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7a935f3ab71a005ce842a783ce051df.js"
+  "/precache-manifest.1044809f4717b360de2473ca3eb532ca.js"
 );
 
 self.addEventListener('message', (event) => {
